96a815e6faa21bac5acaf1eb42f86f294d57c893
GLFTB <zzh1330645214@gmail.com>
2021-02-27 09:33:30 +0800

    telephone的@value的实现

--- src/main/java/cn/glftb/girl/HelloController.java
+++ src/main/java/cn/glftb/girl/HelloController.java
@@ -10,12 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @Autowired
     private StudentProperties studentProperties;
+    @Value(value = "${test.telephone}")
+    private String telephone;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say() {
 
         return studentProperties.getTelephone();
     }
+
+    @RequestMapping(value = "/say/telephone", method = RequestMethod.GET)
+    public String say2() {
+
+        return studentProperties.getTelephone();
+    }
+    @RequestMapping(value = "/say/telephone1", method = RequestMethod.GET)
+    public String say3() {
+
+        return studentProperties.getTelephone();
+    }
+
 }
 
 
